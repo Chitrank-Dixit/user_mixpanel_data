ae28867a09e624f0354b69dcc1810bfcb7f0d571
Chitrank-Dixit <chitrankdixit@gmail.com>
2017-12-21 17:36:15 +0530

    updated the script to get the dump of the details web mixpanel data

---
+++ mixpanel_dump.py
@@ -0,0 +1,35 @@
+import json
+import time
+from config import settings
+ES_HOST = "http://35.154.96.13:9200/"
+
+
+#ES_HOST = "http://127.0.0.1:9200/"
+
+ES_CLIENT = Elasticsearch(
+    [ES_HOST],
+    connection_class=RequestsHttpConnection,
+    http_auth=('elastic', 'flyrobe123'),
+)
+
+
+def dump_elasticsearch_data():
+    page = 2500000
+    details_web = []
+    for i in range(1, 13):
+      query = {
+        "size": 1000000,
+        "from": 1 * page,
+        "query": {
+          "match": {
+            "event": "Details Web"
+          }
+        }
+      }
+      es_data = ES_CLIENT.search(index="fly", doc_type="users", body=query)
+      details_web = details_web + es_data['hits']['hits']
+      page += 100000
+    with open('/Users/chitrankdixit/PycharmProjects/flyrobe-data-science/project/app/mix_panel_data_details_web_'+str(page)+'.json', 'wt') as outfile:
+        json.dump(details_web, outfile)
+
+dump_elasticsearch_data()
