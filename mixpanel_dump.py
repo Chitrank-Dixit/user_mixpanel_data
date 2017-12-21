bb1b8d9ea1ce059229bd5cd0d44dc85b2b328913
Chitrank-Dixit <chitrankdixit@gmail.com>
2017-12-21 17:57:23 +0530

    updated the pwd

--- mixpanel_dump.py
+++ mixpanel_dump.py
@@ -1,6 +1,7 @@
 import json
 import time
-from config import settings
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+#from config import settings
 #ES_HOST = "http://35.154.96.13:9200/"
 
 
@@ -14,7 +15,7 @@ ES_CLIENT = Elasticsearch(
 
 
 def dump_elasticsearch_data():
-    page = 2500000
+    page = 1000000
     details_web = []
     for i in range(1, 13):
       query = {
@@ -28,7 +29,7 @@ def dump_elasticsearch_data():
       }
       es_data = ES_CLIENT.search(index="fly", doc_type="users", body=query)
       details_web = details_web + es_data['hits']['hits']
-      page += 100000
+      page += 1000000
     with open('/home/ubuntu/user_mixpanel_data/mix_panel_data_details_web.json', 'wt') as outfile:
         json.dump(details_web, outfile)
 
